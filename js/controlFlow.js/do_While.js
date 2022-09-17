e57508e2759854051ff2b2d37a3864ac680ea249
faisalahammed044274 <setu2014@gmail.com>
2022-09-17 21:24:39 +0600

    While and Do...While loop

---
+++ js/controlFlow.js/do_While.js
@@ -0,0 +1,21 @@
+//For Loop
+
+for (let i = 0; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
+
+//While Loop (If condition true then it execute)
+
+// let i = 0;
+// while (i <= 5) {
+//   if (i % 2 !== 0) console.log(i);
+//   i++;
+// }
+
+//do-while Loop (do execute atleast one time)
+console.log("Hello");
+let i = 10;
+do {
+  if (i % 12 !== 0) console.log(i);
+  i++;
+} while (i <= 15);
