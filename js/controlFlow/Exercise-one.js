59b882bcc0cd5094ab89c7e05fa31044001fd9cd
faisalahammed044274 <setu2014@gmail.com>
2022-09-18 02:30:56 +0600

    Exercise

--- js/controlFlow/Exercise-one.js
+++ js/controlFlow/Exercise-one.js
@@ -3,8 +3,16 @@
 let number = max(13, 9);
 console.log(number);
 
-function max(num1, num2) {
-  if (num1 > num2) return num1 + " is greater";
-  else if(num1 < num2) return num2 + " is greater" ;
-  else return 'equal';
+// function max(num1, num2) {
+//   if (num1 > num2) return num1 + " is greater";
+//   else if(num1 < num2) return num2 + " is greater" ;
+//   else return 'equal';
+// }
+
+function max(a, b) {
+//   if (a > b) return a;
+//   return b;
+
+//contional operator
+  return (a > b) ? a : b;
 }
