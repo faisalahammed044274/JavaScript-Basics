f1c65ff9afbbac549b94f96bce353c6bfff730a3
faisalahammed044274 <setu2014@gmail.com>
2022-09-18 04:24:53 +0600

    Exercise with effective code example

--- js/controlFlow/Exercise-two.js
+++ js/controlFlow/Exercise-two.js
@@ -1,5 +1,16 @@
-console.log (isLanscape(560,345));
+// Amateur's Code
 
-function isLanscape(width, height){
- return (width > height);
+// let landscape= isLanscape(560,345)
+// console.log(landscape)
+
+// function isLanscape(width, height){
+//  return (width > height) ? "True" : "False";
+// }
+
+// Effective and Professional code
+
+console.log(isLanscape(560, 345));
+
+function isLanscape(width, height) {
+  return width > height;
 }
